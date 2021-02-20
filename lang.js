4547253c5674fee97df4a8b95bd10ac3dd519de9
Sinan Ascioglu <sinan@openprocessing.org>
2021-02-20 14:45:35 -0500

    updated js file with japanese updates

--- lang.js
+++ lang.js
@@ -8,84 +8,84 @@ OP.languageLibraryDefault = {
 		"learnMore": {
 			"description": "Used in links such as in code setting descriptions",
 			"en": "Learn more",
-			"ja": "もっと詳しく知る",
+			"ja": "さらに詳しく",
 			"pt": "Saiba mais"
 		},
 		"learnMoreAbout": {
 			"description": "Used in links such as 'Learn more about Creative Commons'",
 			"en": "Learn more about",
-			"ja": "もっと詳しく知る",
+			"ja": null,
 			"pt": "Saiba mais sobre"
 		},
 		"by": {
 			"description": "Used in phrases like '[Sketch Title] by [User name]'",
 			"en": "by",
-			"ja": "本",
+			"ja": "-",
 			"pt": "por"
 		},
 		"eg": {
 			"description": "Used to give examples with e.g. (for example)'",
 			"en": "e.g., {0}",
-			"ja": "本 {0}",
+			"ja": null,
 			"pt": null
 		},
 		"saveAsFork": {
 			"description": "navigation - when sketch is changed",
 			"en": "Save as Fork",
-			"ja": "スケッチ",
+			"ja": "フォークとして保存",
 			"pt": "Salvar Bifurcação"
 		},
 		"save": {
 			"description": "navigation - when sketch is changed",
 			"en": "Save",
-			"ja": "スケッチ",
+			"ja": "保存",
 			"pt": "Salvar"
 		},
 		"saved": {
 			"description": "navigation - when sketch is saved",
 			"en": "Saved",
-			"ja": "スケッチ"
+			"ja": "保存完了"
 		},
 		"saving": {
 			"description": "navigation - when sketch is saving",
 			"en": "Saving…",
-			"ja": "スケッチ…",
+			"ja": "保存中…",
 			"pt": "Salvando…"
 		},
 		"edit": {
 			"description": "navigation - when info panel is selected",
 			"en": "Edit",
-			"ja": "スケッチ",
+			"ja": "編集",
 			"pt": "Editar"
 		},
 		"submit": {
 			"description": "form submission button",
 			"en": "Submit",
-			"ja": "スケッチ",
+			"ja": "投稿",
 			"pt": "Enviar"
 		},
 		"signin": {
 			"description": "link label for sign in page",
 			"en": "Sign in",
-			"ja": "スケッチ",
+			"ja": "サインイン",
 			"pt": "Cadastrar"
 		},
 		"signout": {
 			"description": "link label for sign out page",
 			"en": "Sign out",
-			"ja": "スケッチ",
+			"ja": "サインアウト",
 			"pt": "Sair"
 		},
 		"join": {
 			"description": "link label asking users to join the website",
 			"en": "Join",
-			"ja": "スケッチ",
+			"ja": "参加する",
 			"pt": "Entrar"
 		},
 		"joinPlus": {
 			"description": "link label asking users to join the Plus+ Membership",
 			"en": "Join Plus+",
-			"ja": "スケッチ Plus+",
+			"ja": "Plus+メンバーになる",
 			"pt": "Entrar Plus+"
 		}
 
@@ -94,13 +94,13 @@ OP.languageLibraryDefault = {
 		"search": {
 			"description": "Placeholder text when search icon is clicked",
 			"en": "Search",
-			"ja": "探す",
+			"ja": "検索",
 			"pt": "Procurar"
 		},
 		"discover": {
 			"description": "navigation item",
 			"en": "Discover",
-			"ja": "発見する",
+			"ja": "発見",
 			"pt": "Descobrir"
 		},
 		"learn": {
@@ -150,199 +150,199 @@ OP.languageLibraryDefault = {
 		"mode-HTMLCSS": {
 			"description": "Shows when user selects pjs mode",
 			"en": "Write HTML, CSS, and JS as you wish.",
-			"ja": "HTMLCSS モード",
+			"ja": "HTML CSS JS モード",
 			"pt": "Escreva HTML, CSS e JS como desejar."
 		},
 		"mode-pjs": {
 			"description": "Shows when user selects pjs mode",
 			"en": "Processingjs is deprecated.",
-			"ja": "Processingjs モード",
+			"ja": "Processingjsモードは非推奨です",
 			"pt": "Processingjs foi descontinuado."
 		},
 		"mode-applet": {
 			"description": "Shows when user selects an legacy Applet mode",
 			"en": "Java doesn't work in browsers anymore.",
-			"ja": "Java モード",
+			"ja": "Javaモードはブラウザでは動作しません",
 			"pt": "Java não funciona mais em navegadores."
 		},
 		"tutorialMode": {
 			"description": "Code Options > sketch > label",
 			"en": "Tutorial Mode",
-			"ja": "モード",
+			"ja": "チュートリアルモード",
 			"pt": "Modo Tutorial"
 		},
 		"tutorialModeDescription": {
 			"description": "Code Options > sketch > label",
 			"en": "Write step-by-step tutorials.",
-			"ja": "モード",
+			"ja": "チュートリアル記事を書く",
 			"pt": "Escreva tutoriais passo-a-passo."
 		},
 		"liveCollaboration": {
 			"description": "Code Options > sketch > label",
 			"en": "Live Collaboration",
-			"ja": "モード",
+			"ja": "ライブコラボレーション",
 			"pt": "Colaboração Ao Vivo"
 		},
 		"liveCollaborationDescription-tutorial": {
 			"description": "Shows when tutorial is enabled",
 			"en": "Live Collaboration is not supported on tutorials.",
-			"ja": "モード",
+			"ja": "ライブコラボレーションはチュートリアルに対応していません",
 			"pt": "Colaboração Ao Vivo não é suportado em tutoriais."
 		},
 		"liveCollaborationDescription-singleTab": {
 			"description": "Shows when sketch has multiple tabs",
 			"en": "Live Collaboration is only supported on sketches with single tab.",
-			"ja": "モード",
+			"ja": "ライブコラボレーションはタブが1つのスケッチのみ対応しています",
 			"pt": "Colaboração Ao Vivo é suportado apenas em esboços com abas únicas."
 		},
 		"liveCollaborationDescription-save": {
 			"description": "Shows when it is a new sketch (not saved)",
 			"en": "Please save your sketch to enable.",
-			"ja": "モード",
+			"ja": "スケッチを保存してください",
 			"pt": "Por favor, salve o esboço para habilitar."
 		},
 		"liveCollaborationDescription-shareURL": {
 			"description": "Shows when enabled",
 			"en": "Share the URL with your class to code together.",
-			"ja": "モード",
+			"ja": "コーディングを楽しむためにクラスにURLをシェアしましょう",
 			"pt": "Compartilhe a URL da sua aula com o código junto."
 		},
 		"liveCollaborationDescription-default": {
 			"description": "Shows when it can be enabled",
 			"en": "Invite your class to code together.",
-			"ja": "モード",
+			"ja": "コーディングを楽しむためにクラスに招待しましょう",
 			"pt": "Convide sua turma para programar juntos."
 		},
 		"showcaseSketch": {
 			"description": "Code Options > sketch",
 			"en": "Showcase Sketch",
-			"ja": "モード",
+			"ja": "ショーケーススケッチ",
 			"pt": "Mostrar seu Esboço."
 		},
 		"showcaseSketchDescription": {
 			"description": "Code Options > sketch",
 			"en": "Centers sketch and matches the background color.",
-			"ja": "モード",
+			"ja": "スケッチを中央に配置して背景色を同じ色にする",
 			"pt": "Centralizar esboço e corresponder a cor de fundo."
 		},
 		"loopProtect": {
 			"description": "Code Options > sketch",
 			"en": "Loop Protection",
-			"ja": "モード",
+			"ja": "ループプロテクション",
 			"pt": "Proteção de Laço"
 		},
 		"loopProtectDescription": {
 			"description": "Code Options > sketch",
 			"en": "Prevents infinite loops that may freeze the sketch.",
-			"ja": "モード",
+			"ja": "スケッチのフリーズを引き起こす無限ループを防止する",
 			"pt": "Previne lanços infinitos que podem congelar seu esboço."
 		},
 		"libraries": {
 			"description": "Code Options > sketch",
 			"en": "Libraries",
-			"ja": "モード",
+			"ja": "ライブラリ",
 			"pt": "Bibliotecas"
 		},
 		"showAll": {
 			"description": "Code Options > sketch > libraries",
 			"en": "Show All",
-			"ja": "モード",
+			"ja": "すべて見る",
 			"pt": "Mostras Todos"
 		},
 		"showLess": {
 			"description": "Code Options > sketch > libraries",
 			"en": "Show Less",
-			"ja": "モード",
+			"ja": "表示を少なくする",
 			"pt": "Mostrar Menos"
 		},
 		"addCustomLibrary": {
 			"description": "Code Options > sketch > libraries",
 			"en": "Add Custom Library",
-			"ja": "モード",
+			"ja": "カスタムライブラリを追加する",
 			"pt": "Adicionar Biblioteca Personalizada"
 		},
 		"pasteURL": {
 			"description": "text input placeholder when adding a custom library",
 			"en": "Paste URL or file name",
-			"ja": "モード",
+			"ja": "URLもしくはファイル名を入力してください",
 			"pt": "Colar URL ou nome do arquivo"
 		},
 		"saveToUpload": {
 			"description": "code settings > files",
 			"en": "Save or fork the sketch to upload files.",
-			"ja": "モード",
+			"ja": "ファイルをアップロードするにはスケッチを保存するかフォークしてください",
 			"pt": "Salve ou bifurque seu esboço para enviar arquivos."
 		},
 		"textSize": {
 			"description": "code settings > editor",
 			"en": "Text Size",
-			"ja": "モード",
+			"ja": "文字サイズ",
 			"pt": "Tamanho do Texto"
 		},
 		"layout": {
 			"description": "code settings > editor",
 			"en": "Layout",
-			"ja": "モード",
+			"ja": "レイアウト",
 			"pt": "Visualização"
 		},
 		"layoutDescription": {
 			"description": "code settings > editor",
 			"en": "This will be the default layout for your sketches",
-			"ja": "モード",
+			"ja": "デフォルトのレイアウト",
 			"pt": "Este será a visualização padrão para seus esboços"
 		},
 		"console": {
 			"description": "code settings > editor",
 			"en": "Console",
-			"ja": "モード",
+			"ja": "コンソール",
 			"pt": "Console"
 		},
 		"consoleDescription": {
 			"description": "code settings > editor",
 			"en": "It will show up when there is an error or print() in code",
-			"ja": "モード",
+			"ja": "コード中のエラーやprint()を表示する",
 			"pt": "Será exibido quando houver um erro ou print() no código"
 		},
 		"linting": {
 			"description": "code settings > editor",
 			"en": "Linting",
-			"ja": "モード",
+			"ja": "リント",
 			"pt": "Linting"
 		},
 		"lintingDescription": {
 			"description": "code settings > editor",
 			"en": "Potential warnings will be displayed as you type",
-			"ja": "モード",
+			"ja": "入力時のエラーや警告を表示する",
 			"pt": "Avisos em potencial serão exibidos conforme você digita"
 		},
 		"lintingDescription-p5js": {
 			"description": "code settings > editor. Shows when sketch is not p5js.",
 			"en": "Disabled: Only available on p5js sketches.",
-			"ja": "モード",
+			"ja": "p5jsのみ使用できます",
 			"pt": "Desabilitado: Disponível somente nos esboços de p5js."
 		},
 		"shortcuts": {
 			"description": "code settings > editor",
 			"en": "Shortcuts",
-			"ja": "モード",
+			"ja": "ショートカット",
 			"pt": "Atalhos"
 		},
 		"seeMoreShortcuts": {
 			"description": "code settings > editor",
 			"en": "Shortcuts",
-			"ja": "モード",
+			"ja": "ショートカット",
 			"pt": "Ver mais atalhos"
 		},
 		"versionsSummarized": {
 			"description": "code settings > versions. Displayed when sketch has more than 20 versions.",
 			"en": "Prior to most recent 20 versions are summarized.",
-			"ja": "モード",
+			"ja": "直近の20バージョンをアーカイブする",
 			"pt": "Prévia das 20 versões mais recentes são resumidas."
 		},
 		"plusPromo": {
 			"description": "code settings > promo. Displayed at the bottom if user is not a Plus+ member",
 			"en": "{0} to add custom libraries, private sketches, and more!",
-			"ja": "{0} モード",
+			"ja": "{0} にカスタムライブラリの追加や、非公開スケッチ、さらに様々な機能が追加できます！",
 			"pt": "{0} adicionar mais bibliotecas personalizadas, esboços privados, e mais!"
 		},
 		"forkInfo": {
@@ -354,31 +354,31 @@ OP.languageLibraryDefault = {
 		"title": {
 			"description": "info > edit panel",
 			"en": "Title",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"description": {
 			"description": "info > edit panel",
 			"en": "Description",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"howToInteract": {
 			"description": "info > edit panel",
 			"en": "How to <br/> interact with it",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"tags": {
 			"description": "info > edit panel",
 			"en": "Tags",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"license": {
 			"description": "info > edit panel",
 			"en": "License",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"license-needPlus": {
@@ -390,97 +390,97 @@ OP.languageLibraryDefault = {
 		"draft": {
 			"description": "info > edit panel",
 			"en": "Draft",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"draftDescription": {
 			"description": "info > edit panel",
 			"en": "won't be listed on feeds but still accessible on your profile",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"whoCanSeeSketch": {
 			"description": "info > edit panel",
 			"en": "Who can see your sketch?",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"whoCanSeeCode": {
 			"description": "info > edit panel",
 			"en": "Who can see the code?",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"whoCanComment": {
 			"description": "info > edit panel",
 			"en": "Who can comment?",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"privacy-anyone": {
 			"description": "info > edit panel",
 			"en": "Anyone",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"privacy-classes": {
 			"description": "info > edit panel",
 			"en": "My Classes",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"privacy-classesDescription": {
 			"description": "info > edit panel",
 			"en": "only the students and teachers in your classes",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"privacy-teachers": {
 			"description": "info > edit panel",
 			"en": "My Teachers",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"privacy-teachersDescription": {
 			"description": "info > edit panel",
 			"en": "only the teachers in your classes",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"privacy-me": {
 			"description": "info > edit panel",
 			"en": "Only Me",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"privateURL": {
 			"description": "info > edit panel",
 			"en": "Private URL",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"privateURLDescription": {
 			"description": "info > edit panel",
 			"en": "You can share your sketch with this URL even if it is set hidden above",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"privateURL-create": {
 			"description": "info > edit panel",
 			"en": "Create",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"privateURL-pleaseSave": {
 			"description": "info > edit panel",
 			"en": "Please save your sketch to create private URL",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		},
 		"deleteSketch": {
 			"description": "info > edit panel",
 			"en": "Delete Sketch?",
-			"ja": "モード",
+			"ja": null,
 			"pt": null
 		}
 	},
@@ -488,145 +488,145 @@ OP.languageLibraryDefault = {
 		"name": {
 			"description": "Edit Profile label",
 			"en": "name",
-			"ja": "モード",
+			"ja": "名前",
 			"pt": "nome"
 		},
 		"location": {
 			"description": "Edit Profile > label",
 			"en": "location",
-			"ja": "モード",
+			"ja": "居住地",
 			"pt": "localização"
 		},
 		"website": {
 			"description": "Edit Profile > label",
 			"en": "website",
-			"ja": "モード",
+			"ja": "ウェブサイト",
 			"pt": "website"
 		},
 		"email": {
 			"description": "Edit Profile > label",
 			"en": "email",
-			"ja": "モード",
+			"ja": "メールアドレス",
 			"pt": "email"
 		},
 		"password": {
 			"description": "Edit Profile > label",
 			"en": "password",
-			"ja": "モード",
+			"ja": "パスワード",
 			"pt": "senha"
 		},
 		"clickToChange": {
 			"description": "Edit Profile > password change link",
 			"en": "Click to Change",
-			"ja": "モード",
+			"ja": "クリックして変更",
 			"pt": "Clique para Mudar"
 		},
 		"language": {
 			"description": "Edit Profile > label",
 			"en": "Language",
-			"ja": "モード",
+			"ja": "言語",
 			"pt": "Idioma"
 		},
 		"learnMoreAbout": {
 			"description": "Edit Profile > language description",
 			"en": "Beta: Learn more about",
-			"ja": "モード",
+			"ja": "ベータ機能: さらに詳しく",
 			"pt": "Beta: Saiba mais sobre"
 		},
 		"multilanguageSupport": {
 			"description": "Edit Profile > language description",
 			"en": "multi-language support",
-			"ja": "モード",
+			"ja": "多言語対応",
 			"pt": "suporte multi-idiomas"
 		},
 		"membership": {
 			"description": "Edit Profile > label",
 			"en": "{membershipType} membership",
-			"ja": "{membershipType} モード",
+			"ja": "{membershipType} メンバーシップ",
 			"pt": "{membershipType} filiação"
 		},
 		"membershipDescription": {
 			"description": "Edit Profile > label. Should use variable {membershipType} instead of 'Plus+'",
 			"en": "Make changes or cancel your {membershipType} Membership",
-			"ja": "モード {membershipType} モード",
+			"ja": "{membershipType} メンバーシップを変更もしくはキャンセルする",
 			"pt": "Alterar ou cancelar sua filiação {membershipType}"
 		},
 		"notifications": {
 			"description": "Edit Profile > label",
 			"en": "Notifications",
-			"ja": "モードモ",
+			"ja": "お知らせ",
 			"pt": "Notificações"
 		},
 		"notificationsDescription": {
 			"description": "Edit Profile > label",
 			"en": "Select when to receive email notifications",
-			"ja": "モードモードモードモード",
+			"ja": "メールのお知らせをいつ受け取るか設定する",
 			"pt": "Selecione quando receber notificações por email"
 		},
 		"deleteAccount": {
 			"description": "Edit Profile > label",
 			"en": "Delete Account",
-			"ja": "モード",
+			"ja": "アカウントの消去",
 			"pt": "Apagar Conta"
 		},
 		"deleteAccountDescription": {
 			"description": "Edit Profile > label",
 			"en": "Just like you'd shoot it with lasers",
-			"ja": "モード",
+			"ja": "消去はレーザーで撃たれるように一瞬です",
 			"pt": "Somente se você for atirar com lasers"
 		},
 		"from": {
 			"description": "user bio",
 			"en": "from",
-			"ja": "モード",
+			"ja": "居住地",
 			"pt": "de"
 		},
 		"editProfile": {
 			"description": "user bio",
 			"en": "Edit Profile",
-			"ja": "モード",
+			"ja": "プロフィールの変更",
 			"pt": "Editar Perfil"
 		},
 		"activity": {
 			"description": "Tab",
 			"en": "Activity",
-			"ja": "モード",
+			"ja": "アクティビティ",
 			"pt": "Atividades"
 		},
 		"sketches": {
 			"description": "Tab",
 			"en": "0 Sketches | 1 Sketch | {n} Sketches",
-			"ja": "0 モード | 1 モード | {n} モード",
+			"ja": "0 スケッチ | 1 スケッチ | {n} スケッチ",
 			"pt": "0 Esboços | 1 Esboço | {n} Esboços"
 		},
 		"classes": {
 			"description": "Tab",
 			"en": "0 Classes | 1 Class | {n} Classes",
-			"ja": "0 モード | 1 モード | {n} モード",
+			"ja": "0 クラス | 1 クラス | {n} クラス",
 			"pt": "0 Aulas | 1 Aula | {n} Aulas"
 		},
 		"curations": {
 			"description": "Tab",
 			"en": "0 Curations | 1 Curation | {n} Curations",
-			"ja": "0 モード | 1 モード | {n} モード",
+			"ja": "0 キュレーション | 1 キュレーション | {n} キュレーション",
 			"pt": "0 Curadorias | 1 Curadoria | {n} Curadorias"
 		},
 		"hearts": {
 			"description": "Tab. Due to copyrights, this phrase should not be anything close to 'like'. Please stick to 'hearts' as in 'you have a place in my heart'",
 			"en": "0 Hearts | 1 Heart | {n} Hearts",
-			"ja": "0 モード | 1 モード | {n} モード",
+			"ja": "0 ハート | 1 ハート | {n} ハート",
 			"pt": "0 Corações | 1 Coração | {n} Corações"
 		},
 		"followers": {
 			"description": "Tab",
 			"en": "0 Followers | 1 Follower |{n} Followers",
-			"ja": "0 モード | 1 モード | {n} モード",
+			"ja": "0 フォロワー | 1 フォロワー | {n} フォロワー",
 			"pt": "0 Seguidores | 1 Seguidor |{n} Seguidores"
 		},
 		"following": {
 			"description": "Tab",
 			"en": "{n} Following",
-			"ja": "モード",
+			"ja": "フォロー中",
 			"pt": "{n} Seguindo"
 		}
 	}
